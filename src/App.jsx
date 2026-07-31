7c4640c1b3aa0bd744385db9a515c9aec3dee521
sagarguftgu <sagarjonwal289@gmail.com>
2026-07-31 19:57:26 +0530

    Create App component with TerminalComponent

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TerminalComponent from './TerminalComponent';
+
+function App() {
+  return (
+    <div style={{ margin: 0, padding: 0, backgroundColor: '#000', minHeight: '100vh' }}>
+      <TerminalComponent />
+    </div>
+  );
+}
+export default App;
